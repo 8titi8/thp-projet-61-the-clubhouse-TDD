116ebe3767c371a7257c52c0cf74520555c36e17
8titi8 <raymathild@gmail.com>
2018-08-06 17:26:35 +0200

    creation model User + 3seeds + page statique home

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+titi = User.create(first_name: 'Mathilde', last_name: 'Ray',
+                  email: 'math@mail.com', password: '123456',
+                  password_confirmation: '123456')
+
+gaga = User.create(first_name: 'Gaelle', last_name: 'Gorgori',
+                  email: 'gaga@mail.com', password: '123456',
+                  password_confirmation: '123456')
+
+toto = User.create(first_name: 'Thomas', last_name: 'Fernandez',
+                  email: 'toto@mail.com', password: '123456',
+                  password_confirmation: '123456')
